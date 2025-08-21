7461a67ab3ced2804a577a6ef9b11c1f4c3a514b
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:18:15 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,9 +1,8 @@
 A = int(input("Enter angle A: "))
 B = int(input("Enter angle B: "))
 C = int(input("Enter angle C: "))
-
-# Check if triangle is valid
 if A + B + C == 180:
     print("The triangle is valid.")
 else:
+
     print("The triangle is not valid.")

8e1130b84765192dd0b5b2dee21ee9b943d4e289
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:18:41 +0530

    Update 13c.py

--- 13c.py
+++ 13c.py
@@ -1,11 +1,11 @@
-# Read two numbers
+
 A = int(input("Enter number A: "))
 B = int(input("Enter number B: "))
 
-# Find maximum
 if A > B:
     print("Maximum number is:", A)
 elif B > A:
     print("Maximum number is:", B)
 else:
+
     print("Both numbers are equal:", A)

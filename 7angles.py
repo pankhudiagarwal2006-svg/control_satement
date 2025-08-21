e6778d69ddee0eec4d15b9ee6d1bcbfbb02cbe04
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:29:37 +0530

    Update 7angles.py

--- 7angles.py
+++ 7angles.py
@@ -1,17 +1,16 @@
 a = int(input("Enter first angle: "))
 b = int(input("Enter second angle: "))
 c = int(input("Enter third angle: "))
-
-# Check if it's a valid triangle
 if a + b + c == 180 and a > 0 and b > 0 and c > 0:
-    # Right Triangle
+
     if a == 90 or b == 90 or c == 90:
         print("Right Triangle")
-    # Obtuse Triangle
+
     elif a > 90 or b > 90 or c > 90:
         print("Obtuse Triangle")
-    # Acute Triangle
+    
     else:
         print("Acute Triangle")
 else:
     print("Not a valid triangle")
+

3176925803970458e559f9f9cdd0aa6d8e403627
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:19:01 +0530

    Update 14c.py

--- 14c.py
+++ 14c.py
@@ -1,11 +1,10 @@
 A = int(input("Enter number A: "))
 B = int(input("Enter number B: "))
 C = int(input("Enter number C: "))
-
-# Find minimum
 if A <= B and A <= C:
     print("Minimum number is:", A)
 elif B <= A and B <= C:
     print("Minimum number is:", B)
 else:
+
     print("Minimum number is:", C)

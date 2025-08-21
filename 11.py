d0fd595957864b8215157c2ef2438d9eca42f5c7
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:17:34 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -3,9 +3,6 @@ B = int(input("Enter number B: "))
 C = int(input("Enter number C: "))
 D = int(input("Enter number D: "))
 E = int(input("Enter number E: "))
-
-# Calculate average
 average = (A + B + C + D + E) / 5
-
-# Print result
 print("Average of 5 numbers is:", average)
+

41f576e8e925f0d1d5043ef2b7de838f033561de
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:53:27 +0530

    Update 8.wheter.py

--- 8.wheter.py
+++ 8.wheter.py
@@ -1,8 +1,7 @@
-# Read age from user
-age = int(input("Enter your age: "))
 
-# Check eligibility
+age = int(input("Enter your age: "))
 if age >= 18:
     print("You are eligible to vote.")
 else:
     print("You are not eligible to vote.")
+

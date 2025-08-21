ff6cbabfb1c7dea5f22c6101573762d0993bf8f9
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:53:49 +0530

    Update 9leapyear.py

--- 9leapyear.py
+++ 9leapyear.py
@@ -1,8 +1,7 @@
-# Read year from user
-year = int(input("Enter a year: "))
 
-# Leap year check
+year = int(input("Enter a year: "))
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(year, "is a Leap Year.")
 else:
+
     print(year, "is Not a Leap Year.")

408474ac4bb074ce21718cf067225768a1a0bcc4
pankhudiagarwal2006-svg <pankhudiagarwal.2006@gmail.com>
2025-08-21 09:19:55 +0530

    Update 1number.py

--- 1number.py
+++ 1number.py
@@ -1,8 +1,7 @@
-# User se number input lena
-num = int(input("Enter a number: "))
 
-# Check divisibility by 7
+num = int(input("Enter a number: "))
 if num % 7 == 0:
     print(f"{num} is divisible by 7.")
 else:
+
     print(f"{num} is not divisible by 7.")
